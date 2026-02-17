d5fab0441f8a5c74d27e2d654c48d32eba1b89bf
koy <koyabsolue@gmail.com>
2026-02-17 23:25:38 +0100

    Fix HTML validation and accessibility issues; update styles

---
+++ script.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
